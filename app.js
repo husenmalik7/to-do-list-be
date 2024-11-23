411d2cb8ec8193fa5985b325e909786a2d8d2e2c
husen <husenmalik7@gmail.com>
2024-11-23 10:00:19 +0700

    build: update config

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3003;
-const cors = require("cors");
+const port = 3003;
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-const routes = require("./src/routes/index");
-app.use("/", routes);
+const routes = require('./src/routes/index');
+app.use('/', routes);
 
 app.listen(port, () => {
-  console.log("your server started on " + port);
+	console.log(`Example app listening on port ${port}`);
 });
 
 module.exports = app;
