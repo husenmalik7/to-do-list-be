286ea25886cf14040aac0f1586b78e64dbd9b045
husen <husenmalik7@gmail.com>
2024-11-23 10:03:24 +0700

    asdasd

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+require('dotenv/config');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3003;
 const cors = require('cors');
+const router = require('./src/routes/index');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', router);
 
-const routes = require('./src/routes/index');
-app.use('/', routes);
+app.get('/', (req, res) => {
+	res.send(`Hello World!`);
+});
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
-
-module.exports = app;
