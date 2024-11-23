9800b86e20ded1fcbc04a604675bec13ea089a3e
husen <husenmalik7@gmail.com>
2024-11-23 09:50:41 +0700

    build: udpate config

--- app.js
+++ app.js
@@ -5,16 +5,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3003;
 const cors = require('cors');
-const router = require('./src/routes/index');
+const routes = require('./src/routes/index');
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/', router);
-
-app.get('/', (req, res) => {
-	res.send(`Hello World!`);
-});
+app.use('/', routes);
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
